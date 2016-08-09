f1d009c930e52606a11cdf26060c45f98e6a3fda
Andrew Tae <andrewtae368@gmail.com>
2016-08-08 23:09:47 -0700

    TODO list

---
+++ cryptogram.rb
@@ -0,0 +1,10 @@
+#TODO: Create answer key (13 pairs of letter switches)
+#TODO: Find a source for text
+#TODO: Convert text with answer key
+#TODO: Think about how to take in answers (Call a letter, enter pair)
+#TODO: Maybe type in the correct answer too?
+#TODO: Figure out how hints are handled
+#TODO: http://tinypic.com/view.php?pic=2qbysl2&s=3#.V6lytFUrKUk reference for typical solution
+#TODO: Reference http://www.puzzlebaron.com/wp-content/uploads/2013/05/cryptos1-500x334.png
+# => Even Harder, for example if you write "O" over "E", "O" should appear over all of the E's
+#	  
